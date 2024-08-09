96a7dd1d14e9ff0e8dfb01d7aa16e0e13831ba9b
jiaxukang <kangjiaxu0605@gmail.com>
2024-08-10 00:57:24 +1000

    fix create

--- app/(auth)/create-account/page.jsx
+++ app/(auth)/create-account/page.jsx
@@ -5,7 +5,7 @@ import { Input } from '@/components/ui/input'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { toast } from 'sonner'
 
 function createAccount() {
@@ -16,10 +16,10 @@ function createAccount() {
     const [loader, setLoader] = React.useState(false);
     useEffect(() => {
         const jwt = sessionStorage.getItem('jwt');
-        if(jwt) {
+        if (jwt) {
             router.push('/');
         }
-    },[])
+    }, [])
     const onCreateAccount = () => {
         setLoader(true);
         GlobalApi.registerUser(username, email, password).then((response) => {
@@ -34,29 +34,29 @@ function createAccount() {
         })
 
     }
-  return (
-      <div className="flex items-baseline justify-center my-20">
-          <div className="flex flex-col items-center justify-center p-10 bg-slate-100 border-gray-200">
-              <Image src="/snake-1.png" alt="logo" width={100} height={100} />
-              <h2 className="font-bold text-3xl">Create An Account</h2>
-              <h2 className="text-gray-500">Enter your Email and Password to Create an Account</h2>
-              <div className="w-full flex flex-col gap-5 mt-7">
-                  <Input placeholder="Username"
-                      onChange={(e) => setUsername(e.target.value)} />
-                  <Input placeholder="name@example.com" onChange={(e) => setEmail(e.target.value)} />
-                  <Input type='password' placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-                  <Button disabled={!(username||email||password) }  onClick={()=>onCreateAccount()}>{ loader?<LoaderIcon className="animate-spin"/>:"Create An Account"}</Button>
-                  <p>
-                    Already have an 
-                      <Link href="/sign-in" className="text-blue-500">
-                          Click here to Sign In
-                      </Link>
-                  </p>
+    return (
+        <div className="flex items-baseline justify-center my-20">
+            <div className="flex flex-col items-center justify-center p-10 bg-slate-100 border-gray-200">
+                <Image src="/snake-1.png" alt="logo" width={100} height={100} />
+                <h2 className="font-bold text-3xl">Create An Account</h2>
+                <h2 className="text-gray-500">Enter your Email and Password to Create an Account</h2>
+                <div className="w-full flex flex-col gap-5 mt-7">
+                    <Input placeholder="Username"
+                        onChange={(e) => setUsername(e.target.value)} />
+                    <Input placeholder="name@example.com" onChange={(e) => setEmail(e.target.value)} />
+                    <Input type='password' placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                    <Button disabled={!(username || email || password)} onClick={() => onCreateAccount()}>{loader ? <LoaderIcon className="animate-spin" /> : "Create An Account"}</Button>
+                    <p>
+                        Already have an
+                        <Link href="/sign-in" className="text-blue-500">
+                            Click here to Sign In
+                        </Link>
+                    </p>
 
-              </div>
-          </div>
-    </div>
-  )
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default createAccount
