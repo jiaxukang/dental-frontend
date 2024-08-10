707f7ca55c8f5f8de7ddcb6b3a056fe2ffec6d94
jiaxukang <kangjiaxu0605@gmail.com>
2024-08-10 22:23:49 +1000

    image fixed

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains: ['https://dental-backend-mf9e.onrender.com',"res.cloudinary.com"]
+        domains: ['https://dental-backend-mf9e.onrender.com','https://res.cloudinary.com']
     }
 };
 
